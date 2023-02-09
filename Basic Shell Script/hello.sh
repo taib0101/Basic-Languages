1c64f4efabc4b09fcdee934cb924e7b0e7a58816
taib0101 <taibvai13@gmail.com>
2023-02-09 23:11:41 +0600

    modified hello.h

--- Basic Shell Script/hello.sh
+++ Basic Shell Script/hello.sh
@@ -1,3 +1,20 @@
 read -p "Enter your name : " x
-
 echo "Hello $x"
+
+read -sp "Enter your password : " y
+echo -e "\n"
+echo "password broke like $y"
+
+read
+echo "tell your nick name $REPLY"
+
+##################################
+# -p represent that you can 	 #
+# read with print line	    	 #
+# -s represent that you can 	 #
+# read with hiding , you do 	 #
+# -sp also but you cant do -ps 	 #
+# if read is forgotten to take   #
+# variable as inpul $REPLY works #
+# in shell built in              #
+##################################
