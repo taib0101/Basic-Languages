15606471f11ef808b605f96a6cf8a7e75c6350fa
taib0101 <taibvai13@gmail.com>
2023-06-23 12:46:42 +0600

    added colors

---
+++ Basic c++/color outputs.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+//colors
+#define black "u001b[30m"
+#define red "u001b[31m"
+#define green "u001b[32m"
+#define yellow "u001b[33m"
+#define blue "u001b[34m"
+#define magenta "u001b[35m"
+#define cyan "u001b[36m"
+#define white "u001b[37m"
+//bright colors
+#define bright_black "\u001b[30;1m"
+#define bright_red "\u001b[31;1m"
+#define bright_green "\u001b[32;1m"
+#define bright_yellow "\u001b[33;1m"
+#define bright_blue "\u001b[34;1m"
+#define bright_magenta "\u001b[35;1m"
+#define bright_cyan "\u001b[36;1m"
+#define bright_white "\u001b[37;1m"
+//background color
+#define background_black "\u001b[40m"
+#define background_red "\u001b[41m"
+#define background_green "\u001b[42m"
+#define background_yellow "\u001b[43m"
+#define background_blue "\u001b[44m"
+#define background_magenta "\u001b[45m"
+#define background_cyan "\u001b[46m"
+#define background_white "\u001b[47m"
+//background bright color
+#define background_bright_black "\u001b[40;1m"
+#define background_bright_red "\u001b[41;1m"
+#define background_bright_green "\u001b[42;1m"
+#define background_bright_yellow "\u001b[43;1m"
+#define background_bright_blue "\u001b[44;1m"
+#define background_bright_magenta "\u001b[45;1m"
+#define background_bright_cyan "\u001b[46;1m"
+#define background_bright_white "\u001b[47;1m"
+
+#define bold "\u001b[1m"
+#define underline "\u001b[4m"
+#define reversed "\u001b[7m"
+//reset
+#define reset "u001b[0m"
+using namespace std;
+
+int main()
+{
+    cout << bright_green << "Hello ";
+    cout << bright_red << "world"<< endl;
+    return 0;
+}
